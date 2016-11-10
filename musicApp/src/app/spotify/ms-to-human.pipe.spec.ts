907fb83a82ff60eabb1a243499300079753e6f1c
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-11-10 10:21:19 +0100

    Dodanie testów zgdonie z oczekiwaniami

--- musicApp/src/app/spotify/ms-to-human.pipe.spec.ts
+++ musicApp/src/app/spotify/ms-to-human.pipe.spec.ts
@@ -4,13 +4,33 @@ import { TestBed, async } from '@angular/core/testing';
 import { MsToHumanPipe } from './ms-to-human.pipe';
 
 describe('Pipe: MsToHuman', () => {
+  let pipe;
+  beforeEach(() => { //mozna podać paremtr done, ktorey umożliwania czekanie za kończoenie jakiś asynichronicznych wywowłań
+    pipe = new MsToHumanPipe();
+  });
+
   it('create an instance', () => {
-    let pipe = new MsToHumanPipe();
     expect(pipe).toBeTruthy();
   });
-  
-  it('czySaMilisekundy', ()=> {
-    expect(true).toBeFalsy;
+
+  // it('czySaMilisekundy', ()=> {
+  //   expect([1]).toBe([1]); // bedzie false, bo tobe sprawdza referencje a nie wartosc
+  // });
+
+  it('Test[38002-38sec]', () => {
+    let result = pipe.transform(38002);
+    expect(result).toBe("38sec");
   });
-  
+
+  it('Test[142389 - 2min22sek]', () => {
+    let result = pipe.transform(142389);
+    expect(result).toBe("2min22sec");
   });
+
+  it('Test[undefinie => _]', () => {
+    let result = pipe.transform(undefined);
+    expect(result).toBe("_");
+  });
+
+
+});
--- musicApp/src/app/spotify/ms-to-human.pipe.ts
+++ musicApp/src/app/spotify/ms-to-human.pipe.ts
@@ -6,7 +6,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MsToHumanPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
+    if (value === undefined || value === null || value === "")  {
+      return "_"
+    }
+
+    const min = Math.floor(value/60000);
+    const sek = Math.floor(value/1000) - min * 60;
+    
+    const minString = min ==0 ? "" : min+"min";
+    const sekString = sek ==0 ? "" : sek+"sec";
+
+    return minString+sekString
   }
 
 }
